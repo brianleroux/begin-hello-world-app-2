fd56c3afc9ac415a24ac3084bd3dd36a1150736a
satyam mishra <satyam.mishra975@gmail.com>
2021-08-02 16:54:55 +0530

    rr

--- index.js
+++ index.js
@@ -1,79 +1,61 @@
-// const puppeteer = require('puppeteer')
-const chromium = require('chrome-aws-lambda')
+const chromium = require('chrome-aws-lambda');
+
+// exports.handler = async (event, context, callback) => {
+//   let result = null;
+//   let browser = null;
+
+//   try {
+    // browser = await chromium.puppeteer.launch({
+    //   args: chromium.args,
+    //   defaultViewport: chromium.defaultViewport,
+    //   executablePath: await chromium.executablePath,
+    //   headless: true,
+    //   ignoreHTTPSErrors: true,
+    // });
+
+//     let page = await browser.newPage();
+
+//     await page.goto(event.url || 'https://google.com');
+
+//     result = await page.title();
+//   } catch (error) {
+//     return callback(error);
+//   } finally {
+//     if (browser !== null) {
+//       await browser.close();
+//     }
+//   }
+
+//   return callback(null, result);
+// };
 async function html_to_pdf(html){
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
-    headless: chromium.headless
+    headless: true,
+    ignoreHTTPSErrors: true,
   });
   const page = await browser.newPage()
   // We set the page content as the generated html by handlebars
   await page.setContent(html)
   // We use pdf function to generate the pdf in the same folder as this file.
   const pdf = await page.pdf({ format: 'A4' ,printBackground:true,margin:false})
-  console.log(pdf)
   await browser.close();
   return pdf
 }
 
-exports.handler = async function http(req) {
-
-  let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
-
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>`
-const pdf = await html_to_pdf('<h1>ded</h1>')
-// console.log(pdf)
+exports.handler = async function http (req) {
+  console.log('Begin API called')
+  const pdf = await html_to_pdf('<h1>Hello miss</h1>')
   return {
+    statusCode: 200,
     headers: {
       'content-type': 'application/pdf',
-      // 'content-disposition': 'attachment; filename=test.pdf'
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
     },
-    statusCode: 200,
-
     body: pdf.toString('base64'),
     isBase64Encoded: true
   }
 }
 
-// Other example responses
-
-/* Forward requester to a new path
-exports.handler = async function http (req) {
-  return {
-    statusCode: 302,
-    headers: {'location': '/about'}
-  }
-}
-*/
-
-/* Respond with successful resource creation, CORS enabled
-let arc = require('@architect/functions')
-exports.handler = arc.http.async (http)
-async function http (req) {
-  return {
-    statusCode: 201,
-    json: { ok: true },
-    cors: true,
-  }
-}
-*/
